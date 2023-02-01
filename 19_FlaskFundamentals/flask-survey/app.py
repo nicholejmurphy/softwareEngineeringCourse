57bfebdc54aae34c6df9b331e386d7fe4e86c137
Nichole-Murphy <nicholejmurphy@gmail.com>
2023-02-01 12:13:55 -0500

    completed flask survey exercise

--- 19_FlaskFundamentals/flask-survey/app.py
+++ 19_FlaskFundamentals/flask-survey/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "value"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 debug = DebugToolbarExtension(app)
 app.debug = True
 
-DEBUG_TB_INTERCEPT_REDIRECTS = False
+DEBUG_TB_INTERCEPT_REDIRECTS = True
 
 responses = []
 
@@ -21,11 +21,21 @@ def home_page():
     return render_template('base.html', survey=satisfaction_survey)
 
 
+@app.route("/start-survey", methods=["POST"])
+def start_survey():
+    """Empties the response list and begins the survey"""
+
+    session["responses"] = []
+    print("********SESSION*******")
+    index = len(session["responses"])
+    return redirect(f"/questions/{index}")
+
+
 @app.route('/questions/<int:index>')
 def survey_questions(index):
     """Survey questions"""
     index = index
-    question_idx = len(responses)
+    question_idx = len(session["responses"])
 
     # Make sure user is answering questions in order. If not, redirect them to the correct question.
     if question_idx != index:
@@ -45,8 +55,10 @@ def answer_question(index):
 
     # Add answer to responses
     ans = request.form['answer']
+    responses = session['responses']
     responses.append(ans)
-    print(responses)
+    session['responses'] = responses
+    print(session["responses"])
 
     # If on the index of question is valid, continue to the next question
     if index <= len(satisfaction_survey.questions) - 1:
@@ -54,10 +66,13 @@ def answer_question(index):
 
     # If user has completed all questions, rediect to 'completed_survey.html'
     else:
-        completed_survey = responses.copy()
+        completed_survey = session["responses"].copy()
+        responses = session["responses"]
         while len(responses) > 0:
             responses.pop()
+        session["responses"] = responses
         print(completed_survey)
+        print(session["responses"])
         return redirect('/completed_survey')
 
 
